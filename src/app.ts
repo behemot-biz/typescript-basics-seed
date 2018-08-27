2c43f6716c7566408827c6dab2d73d459799a0f3
Petra <petra@behemot.biz>
2018-08-27 13:00:36 +0200

    zxc

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,33 @@
-console.log('Hello TypeScript!');
+
+
+// const pizzas = [{ name: 'pepperoni', toppings: ['pepperoni'] }];
+// const mappedPizzas = pizzas.map((pizza, index) => {
+//     return pizza.name.toUpperCase();
+// }); 
+
+// const mappedPizzas = pizzas.map(pizza => pizza.name.toUpperCase());
+
+// console.log(mappedPizzas);
+
+// const pizza = {
+    // name: 'Blazing Inferno',
+    // getName: () => pizza.name,
+// };
+
+// console.log(pizza.getName());
+
+// ********************
+
+// const pizza = {
+//     name: 'Pepperoni',
+//     price: 15,
+//     getName() {
+//         return this.name;
+//     },
+// };
+// console.log(pizza.getName());
+
+// const toppings = ['pepperoni'];
+// const order = {pizza, toppings};
+
+// console.log(order);
